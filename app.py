8607cdf9a155b5d2eea589ed864406aaa033c2b1
Ravindhar Reddy Vantakapu <113596873+vantakapu@users.noreply.github.com>
2022-10-13 17:45:42 +0000

    added apply page

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
-
 
 
 @app.route("/")
@@ -13,8 +12,17 @@ def hello_world():
 
 @app.route("/api/jobs")
 def list_jobs():
-  jobs_list = load_jobs_from_db()
-  return jsonify(jobs_list)
+  jobs = load_jobs_from_db()
+  return jsonify(jobs)
+
+
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+  if not job:
+    return "Not Found", 404
+
+  return render_template('jobpage.html', job=job)
 
 
 if __name__ == "__main__":
